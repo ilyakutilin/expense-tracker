ea38ce2a0500d98963a9a9e9f487e4d8f2dc639b
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-12-03 10:59:37 +0300

    Main.py to include error handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.api.errors import setup_exception_handlers
 from app.api.routers import main_router
+from app.core.settings import settings
 
-app = FastAPI(title="Expense tracker")
 
-app.include_router(main_router)
+def create_application() -> FastAPI:
+    app = FastAPI(
+        title=settings.PROJECT_NAME,
+        version=settings.APP_VERSION,
+        openapi_url=f"{settings.API_V1_STR}/openapi.json",
+        docs_url="/docs" if settings.DEBUG else None,
+        redoc_url="/redoc" if settings.DEBUG else None,
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.BACKEND_CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    setup_exception_handlers(app)
+
+    app.include_router(main_router)
+
+    return app
+
+
+app = create_application()
