a83da4a5db87dafd895a83825c458ccb80f71025
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-12-08 17:16:42 +0300

    Inherit CRUDCurrency from CRUDBase

--- backend/app/crud/currency.py
+++ backend/app/crud/currency.py
@@ -1,13 +1,11 @@
-from typing import Any
-
 from sqlalchemy import and_, select
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.crud.base import CRUDBase
 from app.models import CurrencyORM
 
 
-class CRUDCurrency:
+class CRUDCurrency(CRUDBase):
     async def get_currency_by_code(
         self,
         db_session: AsyncSession,
@@ -19,75 +17,5 @@ class CRUDCurrency:
         result = await db_session.execute(query)
         return result.scalar_one_or_none()
 
-    async def get_currency_by_id(
-        self, db_session: AsyncSession, currency_id: int, include_deleted: bool = False
-    ) -> CurrencyORM | None:
-        query = None
-        if include_deleted:
-            query = select(CurrencyORM).where(CurrencyORM.id_ == currency_id)
-        else:
-            query = select(CurrencyORM).where(
-                and_(CurrencyORM.id_ == currency_id, CurrencyORM.is_active)
-            )
-        result = await db_session.execute(query)
-        return result.scalar_one_or_none()
 
-    async def create_currency(
-        self,
-        db_session: AsyncSession,
-        currency_data: dict[str, Any],
-    ) -> CurrencyORM:
-        try:
-            currency_orm = CurrencyORM(**currency_data)
-
-            db_session.add(currency_orm)
-            await db_session.commit()
-            await db_session.refresh(currency_orm)
-
-            return currency_orm
-
-        except SQLAlchemyError:
-            await db_session.rollback()
-            raise
-
-    async def update_currency(
-        self,
-        db_session: AsyncSession,
-        currency_orm: CurrencyORM,
-        currency_data: dict[str, Any],
-    ) -> CurrencyORM:
-        for field, value in currency_data.items():
-            setattr(currency_orm, field, value)
-
-        try:
-            db_session.add(currency_orm)
-            await db_session.commit()
-            await db_session.refresh(currency_orm)
-
-            return currency_orm
-
-        except SQLAlchemyError:
-            await db_session.rollback()
-            raise
-
-    async def delete_currency(
-        self, db_session: AsyncSession, currency_orm: CurrencyORM, perm: bool = False
-    ) -> None:
-        try:
-            if perm:
-                await db_session.delete(currency_orm)
-            else:
-                currency_orm.is_deleted = True
-            await db_session.commit()
-
-        except SQLAlchemyError:
-            await db_session.rollback()
-            raise
-
-    async def get_all_currencies(self, db_session: AsyncSession) -> list[CurrencyORM]:
-        query = select(CurrencyORM).where(CurrencyORM.is_active)
-        result = await db_session.execute(query)
-        return list(result.scalars().all())
-
-
-currency_crud = CRUDCurrency()
+currency_crud = CRUDCurrency(CurrencyORM)
