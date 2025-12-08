a6a3517303bb0551118483901d99f46d66b6842e
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-12-08 16:53:48 +0300

    Import BaseORM for alembic from models instead of core.db

--- backend/app/core/base.py
+++ backend/app/core/base.py
@@ -1,7 +1,7 @@
 """Imports of base class and all models for Alembic."""
 
-from app.core.db import BaseORM
 from app.models.account import AccountORM
+from app.models.base import BaseORM
 from app.models.currency import CurrencyORM
 from app.models.operation import OperationORM
 from app.models.tag import TagORM
