a2ce7168ca0cc54c16e035608a9f8b1cb383dfb1
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-12-08 16:48:06 +0300

    New BaseORM

---
+++ backend/app/models/base.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+from sqlalchemy import BigInteger, Boolean, DateTime, Identity, false, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+
+class BaseORM(AsyncAttrs, DeclarativeBase):
+    @declared_attr.directive
+    def __tablename__(cls):
+        return cls.__name__.lower().replace("orm", "")
+
+    id_: Mapped[int] = mapped_column(
+        "id",
+        BigInteger,
+        Identity(
+            always=True,
+            start=1,
+            increment=1,
+            cycle=False,
+        ),
+        primary_key=True,
+        sort_order=-1,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=func.now(),
+        sort_order=98,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        server_default=func.now(),
+        onupdate=func.now(),
+        sort_order=99,
+    )
+
+    is_deleted: Mapped[bool] = mapped_column(
+        Boolean,
+        server_default=false(),
+        index=True,
+        sort_order=198,
+    )
+    deleted_at: Mapped[datetime | None] = mapped_column(
+        DateTime,
+        sort_order=199,
+    )
+
+    @hybrid_property
+    def is_active(self) -> bool:  # type: ignore
+        return not self.is_deleted
+
+    @is_active.expression  # type: ignore
+    def is_active(cls):
+        return cls.is_deleted == False  # noqa: E712
+
+    def __repr__(self):
+        """String representation of an ORM Model."""
+        cols = []
+        for col in self.__table__.columns.keys():
+            cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} ({', '.join(cols)})>"
