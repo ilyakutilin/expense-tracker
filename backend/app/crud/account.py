943ee47155213b4b8db14f5cc3c0ba20e6360fd5
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-12-17 10:59:40 +0300

    Move options to CRUDBase

--- backend/app/crud/account.py
+++ backend/app/crud/account.py
@@ -19,22 +19,6 @@ class CRUDAccount(CRUDBase[AccountORM]):
             ),
         )
 
-    async def get_by_id(
-        self, db_session: AsyncSession, obj_id: int, include_deleted: bool = False
-    ) -> AccountORM | None:
-        stmt = select(self.model).where(self.model.id_ == obj_id)
-        if not include_deleted:
-            stmt = stmt.where(self.model.is_active)
-        stmt = stmt.options(*self._get_options())
-        result = await db_session.execute(stmt)
-        return result.unique().scalar_one_or_none()
-
-    async def get_all(self, db_session: AsyncSession) -> list[AccountORM]:
-        stmt = select(self.model).where(self.model.is_active)
-        stmt = stmt.options(*self._get_options())
-        result = await db_session.execute(stmt)
-        return list(result.scalars().all())
-
     async def get_account_by_name(
         self,
         db_session: AsyncSession,
--- backend/app/crud/base.py
+++ backend/app/crud/base.py
@@ -1,8 +1,9 @@
 from typing import Any, Generic, TypeVar
 
-from sqlalchemy import and_, select
+from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm.strategy_options import _AbstractLoad
 
 from app.models.base import BaseORM
 
@@ -13,35 +14,33 @@ class CRUDBase(Generic[ModelType]):
     def __init__(self, model: type[ModelType]) -> None:
         self.model = model
 
+    @classmethod
+    def _get_options(cls) -> tuple[_AbstractLoad, ...]:
+        return tuple()
+
     async def exists(
         self, db_session: AsyncSession, obj_id: int, include_deleted: bool = False
     ) -> bool:
-        stmt = None
-        if include_deleted:
-            stmt = select(self.model.id_).where(self.model.id_ == obj_id)
-        else:
-            stmt = select(self.model.id_).where(
-                and_(self.model.id_ == obj_id, self.model.is_active)
-            )
+        stmt = select(self.model.id_).where(self.model.id_ == obj_id)
+        if not include_deleted:
+            stmt = stmt.where(self.model.is_active)
         result = await db_session.scalar(stmt)
         return result is not None
 
     async def get_by_id(
         self, db_session: AsyncSession, obj_id: int, include_deleted: bool = False
     ) -> ModelType | None:
-        query = None
-        if include_deleted:
-            query = select(self.model).where(self.model.id_ == obj_id)
-        else:
-            query = select(self.model).where(
-                and_(self.model.id_ == obj_id, self.model.is_active)
-            )
-        result = await db_session.execute(query)
+        stmt = select(self.model).where(self.model.id_ == obj_id)
+        if not include_deleted:
+            stmt = stmt.where(self.model.is_active)
+        stmt = stmt.options(*self._get_options())
+        result = await db_session.execute(stmt)
         return result.scalar_one_or_none()
 
     async def get_all(self, db_session: AsyncSession) -> list[ModelType]:
-        query = select(self.model).where(self.model.is_active)
-        result = await db_session.execute(query)
+        stmt = select(self.model).where(self.model.is_active)
+        stmt = stmt.options(*self._get_options())
+        result = await db_session.execute(stmt)
         return list(result.scalars().all())
 
     async def create(
