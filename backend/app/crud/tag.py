591b9a68a2c5904ecdf21c3eb53c5fe207450f67
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-12-18 17:24:50 +0300

    CRUD for Tag

---
+++ backend/app/crud/tag.py
@@ -0,0 +1,19 @@
+from sqlalchemy import and_, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.base import CRUDBase
+from app.models import TagORM
+
+
+class CRUDTag(CRUDBase[TagORM]):
+    async def get_tag_by_name(
+        self,
+        db_session: AsyncSession,
+        name: str,
+    ) -> TagORM | None:
+        query = select(TagORM).where(and_(TagORM.name == name, TagORM.is_active))
+        result = await db_session.execute(query)
+        return result.scalar_one_or_none()
+
+
+currency_crud = CRUDTag(TagORM)
