ebdb5258793dd96d962a064256f307be5efd1f38
Ilya Kutilin <ilyakutilin@yandex.ru>
2026-02-13 16:45:42 +0300

    Add TransactionFilters interface

--- frontend/src/types/transaction.ts
+++ frontend/src/types/transaction.ts
@@ -3,6 +3,32 @@ import type { TagResponseBase } from './tag'
 
 type TransactionType = 'income' | 'expense' | 'transfer' | 'exchange' | 'correction' | 'refund'
 
+const orderByFields = ['id', 'type', 'date', 'is_template'] as const
+type OrderByField = (typeof orderByFields)[number]
+type WithPrefix<T extends string> = T | `-${T}`
+
+export interface TransactionFilters {
+  page?: number
+  page_size?: number
+  type?: TransactionType
+  date_lt?: string
+  date_lte?: string
+  date_gt?: string
+  date_gte?: string
+  comment?: string
+  is_template?: boolean
+  account_id?: number
+  account_id_in?: string
+  amount_lt?: number
+  amount_lte?: number
+  amount_gt?: number
+  amount_gte?: number
+  tag_ids_in?: string
+  tag_ids_all?: string
+  order_by?: WithPrefix<OrderByField>
+  search?: string
+}
+
 export interface TransactionLineBase {
   id: number
   transaction_id: number
